0f3b608cb194084de7383cab7f950778d7025c56
HARIPRASATH A S <98201096+hariprasathas@users.noreply.github.com>
2022-07-05 08:11:52 +0530

    Update ConnectionProvider.java

--- ConnectionProvider.java
+++ ConnectionProvider.java
@@ -13,32 +13,6 @@ public class ConnectionProvider {
 			return null;
 		}
 	}
-    public static void print(){
-    	Connection conn = getConn();
-    	if(conn!=null){
-    		System.out.println("sucess");
-    	}
-    	else
-    		System.out.println("fail");
-    	try{
-//    		 PreparedStatement stmt=conn.prepareStatement("select name from bloodrequest where name=?");
-//			stmt.setString(1, "abc");
-//		     ResultSet resultSet = stmt.executeQuery();  
-//		         
-//		    	System.out.println(resultSet.getString(1));
-    		PreparedStatement ps = conn.prepareStatement("insert into bloodrequest values(?,?,?,?,?)");
-    		ps.setString(1,"hari");
-    		ps.setString(2,"hari");
-    		ps.setString(3,"hari");
-    		ps.setString(4,"hari");
-    		ps.setString(5,"hari");
-    		ps.executeUpdate();
-		    	
-		    
-    	}
-    	catch(Exception e){
-    		System.out.println("eeeeeeeeeeeeeeeeeee");
-    	}
-    }
+    
 }
  
